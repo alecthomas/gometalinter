6fc658529a175b9f5959defbd6e718ef1a9bde5b
Alec Thomas <alec@swapoff.org>
2015-09-24 15:38:51 +1000

    Fix structcheck tests.

--- regression_tests/structcheck_test.go
+++ regression_tests/structcheck_test.go
@@ -10,7 +10,7 @@ type test struct {
 }
 `
 	expected := Issues{
-		{Linter: "structcheck", Severity: "warning", Path: "test.go", Line: 4, Col: 2, Message: "unused struct field test.test.unused"},
+		{Linter: "structcheck", Severity: "warning", Path: "test.go", Line: 4, Col: 2, Message: "unused struct field github.com/alecthomas/gometalinter/regression_tests/.test.unused"},
 	}
 	ExpectIssues(t, "structcheck", source, expected)
 }
