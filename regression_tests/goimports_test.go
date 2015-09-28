1bdc5ff9971a1015d9ba80d2583d45f6c6c30718
Jonathan Gautheron <jgautheron@nexway.com>
2015-09-28 12:53:09 +0200

    Implement goimports

---
+++ regression_tests/goimports_test.go
@@ -0,0 +1,14 @@
+package regression_tests
+
+import "testing"
+
+func TestGoimports(t *testing.T) {
+	source := `
+package test
+func test() {fmt.Println(nil)}
+`
+	expected := Issues{
+		{Linter: "goimports", Severity: "error", Path: "test.go", Line: 1, Col: 0, Message: "file is not goimported"},
+	}
+	ExpectIssues(t, "goimports", source, expected)
+}
