27d000f852fa9c6c0c4a8e3c050710b1c83db186
Martin Chaine <martin@casimir.me>
2016-10-22 12:58:35 +0200

    make tests compatible with forks

--- regressiontests/structcheck_test.go
+++ regressiontests/structcheck_test.go
@@ -1,6 +1,11 @@
 package regressiontests
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
+
+type Empty struct{}
 
 func TestStructcheck(t *testing.T) {
 	t.Parallel()
@@ -10,8 +15,9 @@ type test struct {
 	unused int
 }
 `
+	pkgName := reflect.TypeOf(Empty{}).PkgPath()
 	expected := Issues{
-		{Linter: "structcheck", Severity: "warning", Path: "test.go", Line: 4, Col: 2, Message: "unused struct field github.com/alecthomas/gometalinter/regressiontests/.test.unused"},
+		{Linter: "structcheck", Severity: "warning", Path: "test.go", Line: 4, Col: 2, Message: "unused struct field " + pkgName + "/.test.unused"},
 	}
 	ExpectIssues(t, "structcheck", source, expected)
 }
