41db6241f54b355331eaca498612eb035f433d22
Gareth Lewin <gareth@garethlewin.com>
2016-11-06 08:18:01 -0800

    Make sure tests pass

--- regressiontests/ineffassign_test.go
+++ regressiontests/ineffassign_test.go
@@ -10,7 +10,7 @@ func test() {
 	a := 1
 }`
 	expected := Issues{
-		{Linter: "ineffassign", Severity: "warning", Path: "test.go", Line: 4, Col: 2, Message: "a assigned and not used"},
+		{Linter: "ineffassign", Severity: "warning", Path: "test.go", Line: 4, Col: 2, Message: "ineffectual assignment to a"},
 	}
 	ExpectIssues(t, "ineffassign", source, expected)
 }
--- regressiontests/misspell_test.go
+++ regressiontests/misspell_test.go
@@ -9,8 +9,8 @@ func TestMisSpell(t *testing.T) {
 var a = "langauge"
 `
 	expected := Issues{
-		{Linter: "misspell", Severity: "warning", Path: "test.go", Line: 2, Col: 7, Message: "found \"langauge\" a misspelling of \"language\""},
-		{Linter: "misspell", Severity: "warning", Path: "test.go", Line: 3, Col: 9, Message: "found \"langauge\" a misspelling of \"language\""},
+		{Linter: "misspell", Severity: "warning", Path: "test.go", Line: 2, Col: 7, Message: "\"langauge\" is a misspelling of \"language\""},
+		{Linter: "misspell", Severity: "warning", Path: "test.go", Line: 3, Col: 9, Message: "\"langauge\" is a misspelling of \"language\""},
 	}
 	ExpectIssues(t, "misspell", source, expected)
 }
