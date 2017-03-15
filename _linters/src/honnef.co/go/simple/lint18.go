efcae67197ba0707dc93de203e6b094d441fff37
Bradley Falzon <brad@teambrad.net>
2017-03-15 20:25:56 +1030

    Update dominikh's unused, staticcheck and gosimple paths to go-tools (#258)

--- _linters/src/honnef.co/go/simple/lint18.go
+++
@@ -1,9 +0,0 @@
-// +build go1.8
-
-package simple
-
-import "go/types"
-
-// TODO(dh): use types.IdenticalIgnoreTags once CL 24190 has been merged
-// var structsIdentical = types.IdenticalIgnoreTags
-var structsIdentical = types.Identical
