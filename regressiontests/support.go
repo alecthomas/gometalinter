dcb9b10f17058f62acee5e59c4cb12c77f1d147f
Dan Kaplun <dbkaplun@gmail.com>
2018-05-03 12:07:22 -0400

    Change import "github.com/gotestyourself/gotestyourself/fs" to "gotest.tools/fs" (#472)
    Fixes #471

--- regressiontests/support.go
+++ regressiontests/support.go
@@ -11,7 +11,7 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/gotestyourself/gotestyourself/fs"
+	"gotest.tools/fs"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
--- regressiontests/vet_test.go
+++ regressiontests/vet_test.go
@@ -5,7 +5,7 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/gotestyourself/gotestyourself/fs"
+	"gotest.tools/fs"
 	"github.com/stretchr/testify/assert"
 )
 
