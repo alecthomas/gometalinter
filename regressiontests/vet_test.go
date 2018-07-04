94604582ece1b9cb2abf4eef94a2f0d9a9cdb74f
Sebastiaan van Stijn <github@gone.nl>
2018-07-05 00:12:07 +0200

    Fix TestVet for Go 1.11
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- regressiontests/vet_test.go
+++ regressiontests/vet_test.go
@@ -21,20 +21,32 @@ func TestVet(t *testing.T) {
 			fs.WithFile("file.go", vetFile("excluded"))))
 	defer dir.Remove()
 
-	expected := Issues{
-		{Linter: "vet", Severity: "error", Path: "file.go", Line: 7, Col: 0, Message: "Printf format %d reads arg #1, but call has only 0 args"},
-		{Linter: "vet", Severity: "error", Path: "file.go", Line: 7, Col: 0, Message: "unreachable code"},
-		{Linter: "vet", Severity: "error", Path: "file_test.go", Line: 5, Col: 0, Message: "unreachable code"},
-		{Linter: "vet", Severity: "error", Path: "sub/file.go", Line: 7, Col: 0, Message: "Printf format %d reads arg #1, but call has only 0 args"},
-		{Linter: "vet", Severity: "error", Path: "sub/file.go", Line: 7, Col: 0, Message: "unreachable code"},
-	}
+	var expected Issues
+	version := runtime.Version()
 
-	if version := runtime.Version(); strings.HasPrefix(version, "go1.8") || strings.HasPrefix(version, "go1.9") {
+	switch {
+	case strings.HasPrefix(version, "go1.8"), strings.HasPrefix(version, "go1.9"):
 		expected = Issues{
 			{Linter: "vet", Severity: "error", Path: "file.go", Line: 7, Col: 0, Message: "missing argument for Printf(\"%d\"): format reads arg 1, have only 0 args"},
 			{Linter: "vet", Severity: "error", Path: "file.go", Line: 7, Col: 0, Message: "unreachable code"},
 			{Linter: "vet", Severity: "error", Path: "file_test.go", Line: 5, Col: 0, Message: "unreachable code"},
 			{Linter: "vet", Severity: "error", Path: "sub/file.go", Line: 7, Col: 0, Message: "missing argument for Printf(\"%d\"): format reads arg 1, have only 0 args"},
+			{Linter: "vet", Severity: "error", Path: "sub/file.go", Line: 7, Col: 0, Message: "unreachable code"},
+		}
+	case  strings.HasPrefix(version, "go1.10"):
+		expected = Issues{
+			{Linter: "vet", Severity: "error", Path: "file.go", Line: 7, Col: 0, Message: "Printf format %d reads arg #1, but call has only 0 args"},
+			{Linter: "vet", Severity: "error", Path: "file.go", Line: 7, Col: 0, Message: "unreachable code"},
+			{Linter: "vet", Severity: "error", Path: "file_test.go", Line: 5, Col: 0, Message: "unreachable code"},
+			{Linter: "vet", Severity: "error", Path: "sub/file.go", Line: 7, Col: 0, Message: "Printf format %d reads arg #1, but call has only 0 args"},
+			{Linter: "vet", Severity: "error", Path: "sub/file.go", Line: 7, Col: 0, Message: "unreachable code"},
+		}
+	default:
+		expected = Issues{
+			{Linter: "vet", Severity: "error", Path: "file.go", Line: 7, Col: 0, Message: "Printf format %d reads arg #1, but call has 0 args"},
+			{Linter: "vet", Severity: "error", Path: "file.go", Line: 7, Col: 0, Message: "unreachable code"},
+			{Linter: "vet", Severity: "error", Path: "file_test.go", Line: 5, Col: 0, Message: "unreachable code"},
+			{Linter: "vet", Severity: "error", Path: "sub/file.go", Line: 7, Col: 0, Message: "Printf format %d reads arg #1, but call has 0 args"},
 			{Linter: "vet", Severity: "error", Path: "sub/file.go", Line: 7, Col: 0, Message: "unreachable code"},
 		}
 	}
