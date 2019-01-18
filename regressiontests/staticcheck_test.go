944559026dbfc9ae29c63722190c5e3160422df4
Yaz Saito <ysaito@grailbio.com>
2019-01-18 10:12:33 -0800

    Fix & enhance the static check regtest.

--- regressiontests/staticcheck_test.go
+++ regressiontests/staticcheck_test.go
@@ -9,9 +9,31 @@ func TestStaticCheck(t *testing.T) {
 import "regexp"
 
 var v = regexp.MustCompile("*")
+
+func f(ch chan bool) {
+	var ok bool
+	select {
+	case <- ch:
+	}
+
+	for {
+		select {
+		case <- ch:
+		}
+	}
+
+	if ok == true {
+	}
+}
 `
 	expected := Issues{
+		{Linter: "staticcheck", Severity: "warning", Path: "test.go", Line: 5, Col: 5, Message: "var v is unused (U1000)"},
 		{Linter: "staticcheck", Severity: "warning", Path: "test.go", Line: 5, Col: 27, Message: "error parsing regexp: missing argument to repetition operator: `*` (SA1000)"},
+		{Linter: "staticcheck", Severity: "warning", Path: "test.go", Line: 7, Col: 6, Message: "func f is unused (U1000)"},
+		{Linter: "staticcheck", Severity: "warning", Path: "test.go", Line: 9, Col: 2, Message: "should use a simple channel send/receive instead of select with a single case (S1000)"},
+		{Linter: "staticcheck", Severity: "warning", Path: "test.go", Line: 13, Col: 2, Message: "should use for range instead of for { select {} } (S1000)"},
+		{Linter: "staticcheck", Severity: "warning", Path: "test.go", Line: 19, Col: 2, Message: "empty branch (SA9003)"},
+		{Linter: "staticcheck", Severity: "warning", Path: "test.go", Line: 19, Col: 5, Message: "should omit comparison to bool constant, can be simplified to ok (S1002)"},
 	}
 	ExpectIssues(t, "staticcheck", source, expected)
 }
